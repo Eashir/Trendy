2b7364c421121d21d465331883582263f875c7a1
Eashir <eashir.arafat@gmail.com>
2019-11-08 16:05:13 -0500

    Initial Commit

---
+++ Trendy/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Trendy
+//
+//  Created by Eashir Arafat on 11/8/19.
+//  Copyright © 2019 eashirarafat. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view.
+	}
+
+
+}
+
